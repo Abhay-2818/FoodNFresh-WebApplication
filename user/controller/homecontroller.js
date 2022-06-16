01e64e4ed3730fb53b2b88746c5c44f8cdd230f5
Abhay-2818 <84075203+Abhay-2818@users.noreply.github.com>
2022-06-16 22:59:57 +0530

    Grocery Delivery website

---
+++ user/controller/homecontroller.js
@@ -0,0 +1,14 @@
+exports.homepage = (req,res) => {
+
+    const token = req.cookies.jwt;
+    //console.log(req.params['id']);
+
+    if(token)
+    {
+        res.render("index",{cookie : "generated"});
+    }
+    else{
+        res.render("index");
+    }
+    
+}
